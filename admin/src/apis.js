3865ebc13f859139f26270146eaba1b2afc8e2f1
kimbundi <122777362+kimbundi@users.noreply.github.com>
2025-04-02 07:57:34 +0300

    Update apis.js

--- admin/src/apis.js
+++ admin/src/apis.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://mchama-backend.onrender.com/api";
 
 const getAdminToken = () => {
   const token = localStorage.getItem("adminToken");
